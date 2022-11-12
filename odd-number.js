7b007ab704a68400e4a3aac067800f18a7c31d02
Pratik Shringarpure <114896501+de1uze@users.noreply.github.com>
2022-11-12 09:15:58 +0530

    Add files via upload
    Print odd numbers in an array

---
+++ odd-number.js
@@ -0,0 +1,18 @@
+//odd numbers in array
+var array_odd = function () {
+    odd = [];
+    for (let i = 0; i < 100; i++) {
+
+
+    if (i%2!=0) {
+            odd.push(i);
+        }
+    }
+
+    console.log(odd);
+}
+
+array_odd(); 
+
+
+
