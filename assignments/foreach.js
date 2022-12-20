488a0cdb4a42c67a9e1758433f7c4f8701498fdb
Pratik Shringarpure <114896501+de1uze@users.noreply.github.com>
2022-12-20 20:01:16 +0530

    Add files via upload
    Day-3 assignments ZEN class

---
+++ assignments/foreach.js
@@ -0,0 +1,5 @@
+var names = ["jerry", "tom", "pluto", "micky", "mini"];
+names.forEach(function1);
+function function1(currentValue, index) {
+   console.log("Index in array is: "+index + " ::  Value is: "+currentValue);
+}
---
+++ assignments/resumejson.js
@@ -0,0 +1,52 @@
+let myResume={
+    "basics": {
+      "name": "Pratik Shringarpure",
+      "email": "shringarpure95@gmail.com",
+      "phone": 9423583577,
+      "degree": "B.Tech",
+      "location": {
+        "address": "Pune",
+        "postalCode": 411033,
+        "city": "Pune",
+        "state": "Maharashtra",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/pratik9423/",
+          "github":"https://github.com/de1uze"
+        }
+      ]
+    },
+    
+    "education": [
+      {
+        "institution": "Nutan College Of Engineering",
+        "department": "Computer",
+        "studyType": "fulltime",
+        "batch start year": 2020,
+        "batch end year": 2023,
+        "gpa": 9.4,
+      }
+    ],
+    "skills": [
+      {
+        "name": "python,javascript",
+        "level": "beginer",
+        "project": [
+          "Door unlock system"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Marathi,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "programmer,gamer",
+      }
+    ]
+  }
+  console.log(myResume);
